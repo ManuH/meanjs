23bdd08d97d5178c59e43fac81200a2c87972f18
manuh <hemmanuel44@gmail.com>
2017-01-18 15:53:29 -0300

    fixed unexpected token issue

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.use(stylus.middleware(
 ));
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
+
+
 
 app.get('*', function(req, res) {
     res.render('index');
