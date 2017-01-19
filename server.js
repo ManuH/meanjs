717d3f463936fc923ac897dbf898d9b856eeb347
manuh <hemmanuel44@gmail.com>
2017-01-19 14:25:31 -0300

    added mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     stylus = require('stylus'),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -26,9 +27,19 @@ app.use(stylus.middleware(
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
 
+
+//mongo connection
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('multivision db opened');
+});
+
 app.get('/partials/:partialPath', function(req, res) {
     res.render('partials/' + req.params.partialPath);
 })
+
 
 app.get('*', function(req, res) {
     res.render('index');
